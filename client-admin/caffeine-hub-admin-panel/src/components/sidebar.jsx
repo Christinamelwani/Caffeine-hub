76a43d5c557dd02c8eacc2c0470a402af5809bd2
Christinamelwani <christinarajm@gmail.com>
2022-06-27 23:05:08 +0700

    Created Basic admin layout

---
+++ client-admin/caffeine-hub-admin-panel/src/components/sidebar.jsx
@@ -0,0 +1,23 @@
+export default function Sidebar() {
+  return (
+    <div className="flex bg-black text-white flex-col w-64 h-screen px-4 py-8 overflow-y-auto border-r">
+      <div className="text-xl text-bold text-yellow-500 mb-12">
+        Caffeine Hub
+      </div>
+      <div className="flex flex-col gap-3">
+        <div className="py-2  text-lg hover:bg-neutral-800 cursor-pointer">
+          Dashboard
+        </div>
+        <div className="py-2  text-lg hover:bg-neutral-800 cursor-pointer">
+          Categories
+        </div>
+        <div className="py-2  text-lg hover:bg-neutral-800 cursor-pointer">
+          Register Admin
+        </div>
+        <div className="py-2  text-lg hover:bg-neutral-800 cursor-pointer">
+          Sign out
+        </div>
+      </div>
+    </div>
+  );
+}
