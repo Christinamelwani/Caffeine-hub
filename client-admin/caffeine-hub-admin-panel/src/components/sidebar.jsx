fb82a4ae709eb047c2636793fe2272193585f5e7
Christinamelwani <christinarajm@gmail.com>
2022-06-28 12:35:01 +0700

    Menambahkan fitur Create dan Delete drinks pada admin panel

--- client-admin/caffeine-hub-admin-panel/src/components/sidebar.jsx
+++ client-admin/caffeine-hub-admin-panel/src/components/sidebar.jsx
@@ -1,11 +1,16 @@
-export default function Sidebar() {
+export default function Sidebar({ setDisplayAddForm }) {
   return (
     <div className="flex bg-black text-white flex-col w-64 h-screen px-4 py-8 overflow-y-auto border-r">
       <div className="text-xl text-bold text-yellow-500 mb-12">
         Caffeine Hub
       </div>
       <div className="flex flex-col gap-3">
-        <div className="py-2  text-lg hover:bg-neutral-800 cursor-pointer">
+        <div
+          onClick={() => {
+            setDisplayAddForm(false);
+          }}
+          className="py-2  text-lg hover:bg-neutral-800 cursor-pointer"
+        >
           Dashboard
         </div>
         <div className="py-2  text-lg hover:bg-neutral-800 cursor-pointer">
--- client-admin/caffeine-hub-admin-panel/src/components/tableItems.jsx
+++ client-admin/caffeine-hub-admin-panel/src/components/tableItems.jsx
@@ -1,4 +1,8 @@
-export default function TableItem({ item }) {
+export default function TableItem({ item, deleteDrink }) {
+  const handleDelete = (e) => {
+    const id = e.target.name;
+    deleteDrink(id);
+  };
   return (
     <tr className="border-b odd:bg-white even:bg-purple-50 odd:dark:bg-purple-800 even:dark:bg-purple-700">
       <td className="px-6 py-4 ">{item.id}</td>
@@ -13,7 +17,11 @@ export default function TableItem({ item }) {
         <button className="bg-yellow-500 text-white rounded px-4 py-2">
           Edit
         </button>
-        <button className="bg-red-500 text-white rounded px-4 py-2">
+        <button
+          onClick={handleDelete}
+          name={item.id}
+          className="bg-red-500 text-white rounded px-4 py-2"
+        >
           Delete
         </button>
       </td>
