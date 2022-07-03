2f48139f903da7925f6e12f0eca342d8822c611c
Christinamelwani <christinarajm@gmail.com>
2022-07-03 15:59:32 +0700

    Deploy

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const { errorHandler } = require("./middlewares/errorhandler");
-const port = 3000;
+const port = process.env.PORT;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
