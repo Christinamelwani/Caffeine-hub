ef6601cd00414c031a9a5df5e9c16c7e07764229
Christinamelwani <90821809+Christinamelwani@users.noreply.github.com>
2022-07-03 18:04:40 +0700

    Delete server directory

--- server/routers/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const userController = require("../controllers/user");
-const userRouter = express.Router();
-
-userRouter.post("/register", userController.registerUser);
-userRouter.post("/login", userController.loginUser);
-
-module.exports = userRouter;
